8ed2a0ebb3f87931c044b23d929a84997d2f8781
samanavi98 <snmpathirana@students.nsbm.lk>
2020-01-02 17:56:44 +0530

    db

---
+++ myliquid0.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.1
+ */
+
+/**
+ * Database `myliquid`
+ */
+
+/* `myliquid`.`arrack` */
+$arrack = array(
+  array('user_id' => '0','bott_id' => 'BSA','bott_name' => 'Blue sapphire Arrack.750ml','quantity' => '0','price' => '6350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'CA','bott_name' => 'Ceylon Arrack.700ml','quantity' => '0','price' => '9250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'EAA','bott_name' => 'English apple Arrack.750ml','quantity' => '0','price' => '8100.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'ESA','bott_name' => 'DCSL Extra special Arrack.750ml','quantity' => '0','price' => '8010.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'GCA','bott_name' => 'Governer\'s choice Arrack.750ml','quantity' => '0','price' => '8250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'HAOA','bott_name' => 'Halmilla age old Arrack.750ml','quantity' => '0','price' => '5500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'IDLGL','bott_name' => 'IDL Gold label Arrack.750ml','quantity' => '0','price' => '7900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'KEBA','bott_name' => 'King Edward black Arrack.750ml','quantity' => '0','price' => '5750.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'KTSA','bott_name' => 'Rockland three star Arrack.750ml','quantity' => '0','price' => '8900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'PGA','bott_name' => 'Premium grape Arrack.750ml','quantity' => '0','price' => '6250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'PWCA','bott_name' => 'Paradise white coconut Arrack.750ml','quantity' => '0','price' => '4900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'RDDA','bott_name' => 'Rockland double distilled Arrack.750ml','quantity' => '0','price' => '6500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'ROA','bott_name' => 'Rockland old Arrack.750ml','quantity' => '0','price' => '6010.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'RVX','bott_name' => 'Rockland VX Arrack.750ml','quantity' => '0','price' => '7500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'TMA','bott_name' => 'Tropical mango Arrack.750ml','quantity' => '0','price' => '4750.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'VAT9','bott_name' => 'Vat9 Arrack.750ml','quantity' => '0','price' => '8000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'WDGAA','bott_name' => 'White diamond green apple Arrack.750ml','quantity' => '0','price' => '7950.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'WDL','bott_name' => 'White diam. lemon Arrack.750ml','quantity' => '0','price' => '8010.00','total' => '0.00')
+);
+
+/* `myliquid`.`beer` */
+$beer = array(
+  array('user_id' => '0','bott_id' => 'anchorsmooth','bott_name' => 'Anchor Smooth Beer Can 330Ml','quantity' => '0','price' => '120.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'anchorstrong330','bott_name' => 'Anchor Strong Beer Can 330Ml','quantity' => '0','price' => '200.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'anchorstrong500','bott_name' => 'Anchor Strong Beer Can 500Ml','quantity' => '0','price' => '300.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'baronsstrong500','bott_name' => 'Barons Strong Beer Can 500Ml','quantity' => '0','price' => '340.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'baronsstrong625','bott_name' => 'Barons Strong Brew Beer Bottle 625Ml','quantity' => '0','price' => '380.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'carlsberg330','bott_name' => 'Carlsberg Beer Can 330Ml','quantity' => '0','price' => '140.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'carlsberg625','bott_name' => 'Carlsberg Special Brew Beer Bottle 625Ml','quantity' => '0','price' => '390.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'corona','bott_name' => 'Corona Extra Beer 355Ml','quantity' => '0','price' => '705.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'heinkenbottle','bott_name' => 'Heineken Beer Bottle 330Ml','quantity' => '0','price' => '645.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'heinkencan','bott_name' => 'Heineken Beer Can 330Ml','quantity' => '0','price' => '570.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lionlager','bott_name' => 'Lion Lager Beer Bottle 325Ml','quantity' => '0','price' => '145.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lionstout330','bott_name' => 'Lion Stout Beer Can 330Ml','quantity' => '0','price' => '210.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lionstout500','bott_name' => 'Lion Stout Beer Can 500Ml','quantity' => '0','price' => '330.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lionstrongbottle','bott_name' => 'Lion Strong Beer Bottle 325Ml','quantity' => '0','price' => '215.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'sandopower','bott_name' => 'Sando Power Strong Beer Can 500Ml','quantity' => '0','price' => '290.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'stoutbeer','bott_name' => 'Abc Stout Beer Can 500Ml','quantity' => '0','price' => '330.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'tigerblackbottle','bott_name' => 'Tiger Black Beer Bottle 640Ml','quantity' => '0','price' => '350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'tigerblackcan','bott_name' => 'Tiger Black Beer Can 500Ml','quantity' => '0','price' => '350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'wildapplebottle','bott_name' => 'Ryders Wild Apple Beer Bottle 330Ml','quantity' => '0','price' => '210.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'wildapplecan','bott_name' => 'Ryders Wild Apple Beer Can 500Ml','quantity' => '0','price' => '310.00','total' => '0.00')
+);
+
+/* `myliquid`.`brandy` */
+$brandy = array(
+  array('user_id' => '0','bott_id' => 'ansac','bott_name' => 'Ansac Cognac Brandy.750ml','quantity' => '0','price' => '4900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'bodegas','bott_name' => 'Bodegas torres Brandy.750ml','quantity' => '0','price' => '8000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'calvados','bott_name' => 'Calvados Bushnell Brandy.750ml','quantity' => '0','price' => '4750.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'camuscognac','bott_name' => 'Camus Cognac Brandy.750ml','quantity' => '0','price' => '8010.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'cardenal','bott_name' => 'Cardenal Mendoza Brandy.750ml','quantity' => '0','price' => '6200.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'christianbros','bott_name' => 'Christian-Brothers Brandy.750ml','quantity' => '0','price' => '5750.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'conjure','bott_name' => 'Conjure Cognac Brandy.750ml','quantity' => '0','price' => '6350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'courvoisier','bott_name' => 'Courvoisier Cognac Brandy.750ml','quantity' => '0','price' => '6500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'driftless','bott_name' => 'Driftless Glen Brandy.750ml','quantity' => '0','price' => '8900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'dusse','bott_name' => 'D\'USSE.750ml','quantity' => '0','price' => '4900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'gautier','bott_name' => 'Gautier vs Cognac Brandy.750ml','quantity' => '0','price' => '9250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'germainrobin','bott_name' => 'Germain Robin Brandy.750ml','quantity' => '0','price' => '8250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jacques','bott_name' => 'Jacques Cardin Brandy.750ml','quantity' => '0','price' => '8100.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jacquins','bott_name' => 'Jacquins Brandy.750ml','quantity' => '0','price' => '7900.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'korbel','bott_name' => 'Korbel Brandy.750ml','quantity' => '0','price' => '6010.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'landy','bott_name' => 'Landy.750ml','quantity' => '0','price' => '6250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'meukow','bott_name' => 'Meukow-Brand Brandy.750ml','quantity' => '0','price' => '7450.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'mrboston','bott_name' => 'Mr Boston Brandy.750ml','quantity' => '0','price' => '7950.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'presidente','bott_name' => 'Presidente Brandy.750ml','quantity' => '0','price' => '7500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'remymartin','bott_name' => 'Remy Martin Cognac Brandy.750ml','quantity' => '0','price' => '5500.00','total' => '0.00')
+);
+
+/* `myliquid`.`vodka` */
+$vodka = array(
+  array('user_id' => '0','bott_id' => 'absolute','bott_name' => 'Absolute','quantity' => '0','price' => '8000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'beluga','bott_name' => 'Beluga Vodka','quantity' => '0','price' => '12500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'chopin','bott_name' => 'Chopin','quantity' => '0','price' => '7000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'clubform','bott_name' => 'Club form','quantity' => '0','price' => '9000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'flirt','bott_name' => 'FLIRT Vodka','quantity' => '0','price' => '8200.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'flirtred','bott_name' => 'FLIRT Vodka (RED)','quantity' => '0','price' => '8100.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'grants','bott_name' => 'Grants','quantity' => '0','price' => '9500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'greenlabel','bott_name' => 'Green label','quantity' => '0','price' => '10000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'greygoose','bott_name' => 'Grey Goose','quantity' => '0','price' => '8500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'iregal','bott_name' => 'Iregal','quantity' => '0','price' => '12000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'moulin','bott_name' => 'Moulin Vodka','quantity' => '0','price' => '12000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'smirnoff','bott_name' => 'Smiroff','quantity' => '0','price' => '13000.00','total' => '0.00')
+);
+
+/* `myliquid`.`whiskey` */
+$whiskey = array(
+  array('user_id' => '0','bott_id' => 'ballantines','bott_name' => 'Ballantines Finest Whisky','quantity' => '0','price' => '7600.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jackdaniels','bott_name' => 'Jack Daniels','quantity' => '0','price' => '11500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jjirish','bott_name' => 'John Jamesons Irish Whiskey','quantity' => '0','price' => '9000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jwblack','bott_name' => 'Johnnie Walker Black Label','quantity' => '0','price' => '12350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jwdb','bott_name' => 'Johnnie Walker Double Black','quantity' => '0','price' => '15020.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jwred','bott_name' => 'Johnnie Walker Red Label','quantity' => '0','price' => '8080.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'kavalan','bott_name' => 'KAVALAN EX-BOURBON OAK SINGLE MALT WHISKY','quantity' => '0','price' => '8250.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'kinkarden','bott_name' => 'Kincarden Blended Scotch Whisky.750ml','quantity' => '0','price' => '8010.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lochlomond','bott_name' => 'Loch Lomond Original Single Malt Scotch Whisky','quantity' => '0','price' => '13200.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'macgregor','bott_name' => 'Clan Macgregor','quantity' => '0','price' => '5300.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'talisker','bott_name' => 'Talisker Malt','quantity' => '0','price' => '21000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'vat','bott_name' => 'VAT 69','quantity' => '0','price' => '6085.00','total' => '0.00')
+);
+
+/* `myliquid`.`wine` */
+$wine = array(
+  array('user_id' => '0','bott_id' => 'alamos','bott_name' => 'Alamos Malbec','quantity' => '0','price' => '9000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'beringer','bott_name' => 'Beringer','quantity' => '0','price' => '11500.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'echofalls','bott_name' => 'Echo Falls','quantity' => '0','price' => '9000.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'jacobs','bott_name' => 'Jacobs Creek','quantity' => '0','price' => '12350.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'lucien','bott_name' => 'Lucien Albrecht','quantity' => '0','price' => '7600.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'moscato','bott_name' => 'Moscato','quantity' => '0','price' => '18080.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'ortense','bott_name' => 'Ortense Rosso','quantity' => '0','price' => '8300.00','total' => '0.00'),
+  array('user_id' => '0','bott_id' => 'saracco','bott_name' => 'Saracco','quantity' => '0','price' => '13085.00','total' => '0.00')
+);
